8317cb8aa6b53c38e84b59092f4cada370c9386d
Ítallo Silva <itallo.silva@ccc.ufcg.edu.br>
2023-04-01 09:58:07 -0300

    First version using OpenAPI and Flask

---
+++ chat.py
@@ -0,0 +1,34 @@
+import os
+import openai
+from flask import Flask, request
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.organization = os.getenv("ORG_ID")
+
+app = Flask(__name__)
+
+def generate_song(genre, theme, style):
+    prompt = f"Generate the lyrics and chord notation of a {genre} song about {theme} with the {style} style. You must put the chords where the chord change should occur."
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    return completion.choices[0].message
+
+
+@app.route("/generate", methods = ['POST'])
+def generate():
+
+    req_data = request.get_json()
+    genre = req_data['genre']
+    theme = req_data['theme']
+    style = req_data['style']
+
+    return generate_song(genre, theme, style)
+
+app.run(host='0.0.0.0', port=5000)
